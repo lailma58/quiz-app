756c06db39bcf6994c5e5f2cb579c89364689f5e
Lailma <lailmamahmood58@gmail.com>
2025-08-03 12:33:49 +0500

    Add files via upload

---
+++ src/questions.js
@@ -0,0 +1,29 @@
+const questions = [
+  {
+    question: 'What is the capital of France?',
+    options: ['London', 'Berlin', 'Paris', 'Madrid'],
+    correctAnswer: 2,
+  },
+  {
+    question: 'Which planet is known as the Red Planet?',
+    options: ['Earth', 'Mars', 'Jupiter', 'Venus'],
+    correctAnswer: 1,
+  },
+  {
+    question: 'Which of these is not a fruit?',
+    options: ['Tomato', 'Carrot', 'Banana', 'Apple'],
+    correctAnswer: 1,
+  },
+  {
+    question: 'What is the largest ocean on Earth?',
+    options: ['Atlantic Ocean', 'Indian Ocean', 'Arctic Ocean', 'Pacific Ocean'],
+    correctAnswer: 3,
+  },
+  {
+    question: 'What gas do plants absorb from the atmosphere?',
+    options: ['Oxygen', 'Carbon Dioxide', 'Nitrogen', 'Hydrogen'],
+    correctAnswer: 1,
+  },
+];
+
+export default questions;
